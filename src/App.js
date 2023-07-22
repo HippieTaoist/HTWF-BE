75ad498bb9305f249c34c28f7ebdd2934397b141
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-07-22 09:21:14 -0600

    moving foraward adding components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
+import { CreateBook } from "./components/CreateBook";
+import { ShowBookList } from "./components/ShowBookDetails";
+import { ShowBookDetails } from "./components/ShowBookDetails";
+import { UpdateBookInfo } from "./components/UpdateBookInfo";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<ShowBookList />} />
+          <Route path="/create-book" element={<CreateBook />} />
+          <Route path="/edit-book:id" element={<UpdateBookInfo />} />
+          <Route path="/show-book/:id" element={<ShowBookDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
