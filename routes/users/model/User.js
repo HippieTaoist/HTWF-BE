4b89a83e966d708e9662d7ccd870b960cddb976b
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-08-11 07:17:36 -0600

    updated user model to include admin levels along with initial setup for login and password

--- routes/users/model/User.js
+++ routes/users/model/User.js
@@ -6,3 +6,49 @@ const {
   isStrongPassword,
   isAlphanumeric,
 } = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    nameFirst: {
+      type: String,
+      required: true,
+      validate: [isAlpha, 'Only letters in the First Name'],
+    },
+    nameLast: {
+      type: String,
+      required: true,
+      validate: [isAlpha, 'Only letters in the Last Name'],
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [isAlphanumeric, 'Username must be alphanumeric'],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [
+        isEmail,
+        'Email must be a valid email. ThisIs@vValidEmail.com',
+      ],
+    },
+    password: {
+      type: String,
+      validate: [
+        isStrongPassword,
+        'Make A Stronger Password. (At least one NUMBER & on SPECIAL CHARACTER & one UPPERCASE LETTER & one LOWERCASE LETTER, with at least 8 characters in total.',
+      ],
+    },
+    userLevel: {
+      type: String,
+      enum: ['basic', 'subscription', 'Adm!n'],
+      default: 'basic',
+    },
+    adminLevel: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
