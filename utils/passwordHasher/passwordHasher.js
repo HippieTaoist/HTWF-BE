7c14f758fbd16ae7e65a9d22ebe6ad959f9089f9
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-08-11 07:54:57 -0600

    update comments to explain what file is doing

--- utils/passwordHasher/passwordHasher.js
+++ utils/passwordHasher/passwordHasher.js
@@ -1,16 +1,24 @@
+// bringin bcrypt to add some salt and hash together and encrypt passowrd
 const bcrypt = require('bcrypt');
 
+// create async function due to awaits from bcrypt hashing
 async function passwordHasher(password) {
+  // display function called
   console.log('');
   console.log('');
   console.log('          passwordHasher Called');
   console.log('');
   console.log('');
 
+  // declare and define salt with a auto encryption from bcrypt
   let salt = await bcrypt.genSalt(10);
-  let passwordHasher = await bcrypt.hash(password, salt);
 
-  console.log(passwordhashed);
+  //  declare and define passwordHashed with a combo of salt and password
+  let passwordHashed = await bcrypt.hash(password, salt);
+
+  //   display hashed password  in console
+  console.log(passwordHashed);
 }
 
+// SEND IT!
 module.exports = passwordHasher;
