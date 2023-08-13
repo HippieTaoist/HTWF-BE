482607a4eb29c200b1b21fe561ff4229ee18f490
Shawn Kittel <shawn.m.kittel@gmail.com>
2023-08-13 09:30:27 -0600

    finished routes and included module exports

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const { middlewareJWT } = require('./lib/middlewareJwt/middlewareJWT');
+const { middlewareJwt } = require('./lib/middlewareJwt/middlewareJWT');
 
 const {
   userGet,
@@ -12,3 +12,19 @@ const {
   userUpdate,
   userDelete: userDelete,
 } = require('../users/controller/usersController');
+
+router.get('/', middlewareJwt, usersGet);
+
+router.get('/user-get/:username', middlewareJwt, userGet);
+
+router.post('/user-create', userCreate);
+
+router.post('user-login', userLogin);
+
+router.get('/user-profile', middlewareJwt, userProfile);
+
+router.put('/user-update', middlewareJwt, userUpdate);
+
+router.delete('/user-delete', middlewareJwt, userDelete);
+
+module.exports = router;
